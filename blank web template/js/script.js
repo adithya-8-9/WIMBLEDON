c859190c43363a0bb255d8c9f152a29838729819
adithya-8-9 <adithyasrinivasan2022@gmail.com>
2024-12-26 01:24:06 -0600

    Add files via upload

---
+++ blank web template/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$(".more").mouseenter(function() {
+		$(".overlay").fadeIn();
+	});
+	$(".more").mouseleave(function() {
+		$(".overlay").fadeOut();
+	});
+
+	
+
+});
